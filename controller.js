cfb799f8cc4afab2831e39bff26ad999d4b12bbc
IgMiras <im.miras@unesp.br>
2024-06-12 12:36:35 -0300

    first commit

---
+++ controller.js
@@ -0,0 +1,32 @@
+import KafkaConfig from './config.js';
+import redis from 'redis';
+import { promisify } from 'node:util';
+
+const redisClient = redis.createClient();
+const lpushAsync = promisify(redisClient.lpush).bind(redisClient);
+
+const sendMessageToKafka = async (req, res) => {
+	try {
+		const occurrence = req.body;
+
+		// add occurrence to Redis
+		await lpushAsync('ocurrences', JSON.stringify(occurrence));
+
+		const kafkaConfig = new KafkaConfig();
+		const messages = [
+			{ key: occurrence.id, value: JSON.stringify(occurrence) },
+		];
+
+		await kafkaConfig.produce('my-topic', messages);
+
+		res.status(200).json({
+			message: 'Message sucessfully send!',
+		});
+	} catch (err) {
+		console.log(err);
+	}
+};
+
+const controllers = { sendMessageToKafka };
+
+export default controllers;
---
+++ index.js
@@ -0,0 +1,48 @@
+import express from 'express';
+import controllers from './controller.js';
+import KafkaConfig from './config.js';
+import { createServer } from 'node:http';
+import { Server } from 'socket.io';
+import redis from 'redis';
+import { promisify } from 'node:util';
+
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+app.use(express.json());
+const server = createServer(app);
+const io = Server(server);
+
+const redisClient = redis.createClient();
+const getAsync = promisify(redisClient.get).bind(redisClient);
+const setAsync = promisify(redisClient.set).bind(redisClient);
+const lpushAsync = promisify(redisClient.lpush).bind(redisClient);
+const lrangeAsync = promisify(redisClient.lrange).bind(redisClient);
+
+app.post('/api/send', controllers.sendMessageToKafka, io.emit('newOcurrence'));
+
+// Evento de conexão do Socket.io
+io.on('connection', async (socket) => {
+	console.log('A user connected');
+
+	// Carregar todas as ocorrências existentes do Redis
+	const ocorrencias = await lrangeAsync('ocurrences', 0, -1);
+	const parsedOccurrences = ocorrencias.map((occ) => JSON.parse(occ));
+
+	socket.emit('loadOcurrences', parsedOccurrences);
+
+	socket.on('disconnect', () => {
+		console.log('User disconnected');
+	});
+});
+
+// consume from topic "test-topic"
+const kafkaConfig = new KafkaConfig();
+kafkaConfig.consume('my-topic', (value) => {
+	console.log('valor consumido: ', value);
+	io.emit('newOcurrence', value);
+});
+
+app.listen(PORT, () => {
+	console.log(`Server is running on port ${PORT}`);
+});
